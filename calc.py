4238b3e1ee9fe10ee72b13729d31881d56244e75
ryan Kariuki <130473179+raykaris@users.noreply.github.com>
2025-03-11 21:20:10 +0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,23 @@
+print("Ryan's Calculator")
+
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter Second number: \n"))
+
+print("Select an operator:")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Division (/)")
+print("4. Multiplication (*)")
+
+operator = input("Enter operator(either the number or symbol):")
+
+if operator == "1" or operator == "+":
+    print("num1 + num2 =", num1 + num2)
+elif operator == "2" or operator == "-":
+    print("num1 - num2 =",num1 - num2)
+elif operator == "3" or operator == "/":
+    print("num1 / num2 =",num1 / num2)
+elif operator == "4" or operator == "*":
+    print("num1 * num2 =",num1 * num2)
+else:
+    print("Invalid input, please enter a valid operator")
